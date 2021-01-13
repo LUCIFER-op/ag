05990fe0423cac73b3b2ad3fc71452feabc8da60
LUCIFER-op <76162901+LUCIFER-op@users.noreply.github.com>
2021-01-13 18:49:10 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,31 @@
+class Chain
+{
+constructor(bodyA, bodyB)
+{
+    var options = {
+        bodyA : bodyA,
+        bodyB : bodyB,
+        stiffness : 0.04,
+        length : 10
+    }
+    this.chain= Matter.Constraint.create(options);
+    World.add(world,this.chain);
+
+}
+display()
+{
+   var pos1= this.chain.bodyA.position;
+   
+   var pos2= this.chain.bodyB.position;
+   
+  strokeWeight(2);
+   line(pos1.x,pos1.y, pos2.x, pos2.y)
+
+
+
+
+
+
+
+}
+}
